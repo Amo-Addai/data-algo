463138db9b9acd632c760f7048107f3c0f086ede
Amo-Addai <kwadwoamoad@gmail.com>
2023-07-15 13:05:22 +0100

    .

--- Data-Algo - C++.cpp
+++ Data-Algo - C++.cpp
@@ -11,92 +11,102 @@ MemMan, ..
 
 */
 
-
 ////////////////////////////////////////
 //  SEARCHING ALGO'S
 ////////////////////////////////////////
 
-class SearchingAlgorithms {
+class SearchingAlgorithms
+{
 
-    private:
+private:
+    int i;
 
-        int i;
+public:
+    SearchingAlgorithms::SearchingAlgorithms() {}
+    SearchingAlgorithms::~SearchingAlgorithms() {}
 
-    public: 
-    
-        SearchingAlgorithms::SearchingAlgorithms () {}
-        SearchingAlgorithms::~SearchingAlgorithms () {}
-
-        int linearSearch(int a[], int x) {
-            for (int i = 0; i < sizeof(a); i++) {
-                if (x == a[i]) return i;
-            }
+    int linearSearch(int a[], int x)
+    {
+        for (int i = 0; i < sizeof(a); i++)
+        {
+            if (x == a[i])
+                return i;
         }
+    }
 
-        int binarySearch(int a[], int x) {
-            // a = rsort(a);
-            if (sizeof(a) == 0) return;
+    int binarySearch(int a[], int x)
+    {
+        // a = rsort(a);
+        if (sizeof(a) == 0)
+            return;
 
-            int rBinarySearch(int a[], int x) {
-                if (sizeof(a) == 0) return;
-                int m = sizeof(a) / 2;
-                if (x < a[m]) return rBinarySearch(a, x); // slice a
-                else if (x > a[m]) return rBinarySearch(a, x); // slice a
-                else return m;
-            }
+        int f = 0, l = sizeof(a) - 1, m;
+        rBinarySearch(a, 7);
+        rBinarySearch(a, 7, f, l);
 
-            int rBinarySearch2p(int a[], int x, int f, int l) {
-                if (sizeof(a) == 0) return;
-                int m = sizeof(a) / 2;
-                if (x < a[m]) return rBinarySearch(a, x, f, m - 1);
-                else if (x > a[m]) return rBinarySearch(a, x, m + 1, l);
-                else return m;
-            }
-
-            int f = 0;
-            int l = sizeof(a) - 1;
-            int m;
-            rBinarySearch(a, 7); rBinarySearch(a, 7, f, l);
-
-            while (f < l) {
-                m = (f + l) / 2;
-                if (x < a[m]) l = m - 1;
-                else if (x > a[m]) l = m + 1;
-                else return m;
-            }
+        while (f < l)
+        {
+            m = (f + l) / 2;
+            if (x < a[m])
+                l = m - 1;
+            else if (x > a[m])
+                l = m + 1;
+            else
+                return m;
         }
+    }
 
+    int rBinarySearch(int a[], int x)
+    {
+        if (sizeof(a) == 0)
+            return;
+        int m = sizeof(a) / 2;
+        if (x < a[m])
+            return rBinarySearch(a, x); // slice a
+        else if (x > a[m])
+            return rBinarySearch(a, x); // slice a
+        else
+            return m;
+    }
+
+    int rBinarySearch(int a[], int x, int f, int l)
+    {
+        if (sizeof(a) == 0)
+            return;
+        int m = sizeof(a) / 2;
+        if (x < a[m])
+            return rBinarySearch(a, x, f, m - 1);
+        else if (x > a[m])
+            return rBinarySearch(a, x, m + 1, l);
+        else
+            return m;
+    }
 };
-
+// returnType SearchingAlgorithms::method(args) {} // defined outside of class scope
 
 ////////////////////////////////////////
 //  SORTING ALGO'S
 ////////////////////////////////////////
 
-class SortingAlgorithms {
+class SortingAlgorithms
+{
 
-    public: 
-    
-        SortingAlgorithms::SortingAlgorithms () {}
-        SortingAlgorithms::~SortingAlgorithms () {}
-
+public:
+    SortingAlgorithms::SortingAlgorithms() {}
+    SortingAlgorithms::~SortingAlgorithms() {}
 };
-
-
 
 ////////////////////////////////////////
 //  OTHER ALGO'S
 ////////////////////////////////////////
 
-// 
-
-
-
+//
 
 ////////////////////////////////////////
 //  TEST CASES
 ////////////////////////////////////////
 
-void main(int argc, char argv[]) {
+void main(int argc, char argv[])
+{
     cout << "Hello, World!" << endl;
 }
