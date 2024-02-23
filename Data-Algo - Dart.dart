e47375e4daefc8252d565a439e20711951f19795
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2024-02-23 16:41:29 +0000

    .

--- Data-Algo - Dart.dart
+++ Data-Algo - Dart.dart
@@ -14,39 +14,44 @@ Closures, ..
 
 class Searching {
 
-  Searching([this.i]);
+  var _i;
 
-  int linearSearch(Array<T> a, T x) {
+  Searching([this._i]);
+
+  int? linearSearch(List<dynamic> a, int x) {
     for (var i = 0; i < a.length; i++) { // length
       if (x == a[i]) return i;
     }
+    return null;
   }
 
-  int binarySearch(Array<T> a, T x) {
+  int? binarySearch(List<dynamic> a, int x) {
     // a.sort()
     if (a.length == 0) return null;
 
-    Function rBinarySearch = (Array<T> a, T x) {
+    Function rBinarySearch = (List<dynamic> a, int x) {
       if (a.length == 0) return null;
-      var m = a.length / 2;
+      int m = (a.length / 2).truncate();
       if (x < a[m]) return rBinarySearch(a, x); // slice a
       else if (x > a[m]) return rBinarySearch(a, x); // slice a
       else return m;
-    }
+    };
 
-    Function rBinarySearch2p = (Array<T> a, T x, int f, int l) {
+    Function rBinarySearch2p = (List<dynamic> a, int x, int f, int l) {
       if (a.length == 0) return null;
-      var m = (f + l) / 2;
+      int m = ((f + l) / 2).truncate();
       if (x < a[m]) return rBinarySearch2p(a, x, f, m - 1);
       else if (x > a[m]) return rBinarySearch2p(a, x, m + 1, l);
       else return m;
-    }
+    };
 
-    int f = 0, l = a.length - 1, m;
+    int f = 0;
+    int l = a.length - 1;
+    int m;
     rBinarySearch(a, 7); rBinarySearch2p(a, 7, f, l);
 
     while (f < l) {
-      m = (f + l) / 2;
+      m = ((f + l) / 2).truncate();
       if (x < a[m]) l = m - 1;
       else if (x > a[m]) f = m + 1;
       else return m;
@@ -63,7 +68,9 @@ class Searching {
 
 class Sorting {
 
-  Sorting([this.i]);
+  var _i;
+
+  Sorting([this._i]);
 
 }
 
